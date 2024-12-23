68cf728b0a00b8525bdc4f87cf0ac1e9b268bc30
Inesh-Reddy <ineshreddy007@gmail.com>
2024-12-23 12:32:31 +0530

    Initial commit

---
+++ src/main/java/com/inesh/fakestorethirdpartyapis/FakeStoreThirdpartyApIsApplication.java
@@ -0,0 +1,13 @@
+package com.inesh.fakestorethirdpartyapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreThirdpartyApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeStoreThirdpartyApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inesh/fakestorethirdpartyapis/FakeStoreThirdpartyApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inesh.fakestorethirdpartyapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreThirdpartyApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
